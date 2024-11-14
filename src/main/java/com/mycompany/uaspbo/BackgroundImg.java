2e2bcd8d389974be3790a6097345ac0538322512
Imagine0123 <127961339+Imagine0123@users.noreply.github.com>
2024-11-15 00:48:51 +0700

    11/15 2024

---
+++ src/main/java/com/mycompany/uaspbo/BackgroundImg.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uaspbo;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author lenovo
+ */
+public class BackgroundImg {
+    ImageIcon background = new ImageIcon(getClass().getResource("/images/bglogin.png"));
+
+    public ImageIcon getBackground() {
+        return background;
+    }
+    
+}
